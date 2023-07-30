c45806986cb7aa8fa3b5ffca4d2a2effb2bfa5ab
JohnOrozcoC <john.ocardona@gmail.com>
2023-07-30 12:08:35 -0500

    Cambio de mensaje Hello World

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Colombia, Antioquia, Envigado!";
 console.log(hello());
 exports.hello = hello;
